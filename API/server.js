7b8aba01519f295a1073e45f946d7691cb920ce9
bharti3bk <bharti3bk@gmail.com>
2019-09-13 10:33:26 -0700

    Added Endpoints

--- API/server.js
+++ API/server.js
@@ -1,7 +1,12 @@
-const express = require("express");
+const express = require("express"); 
+const router = require('../Routers/dataRouter.js')
 
 const server = express();
 
-server.use(express.json());
+server.use(express.json()); 
+server.use('/api/projects' , router);  
+server.get('/' , (req ,res) => {
+    console.log("Hello from Express...")
+})
 
 module.exports = server;
--- Routers/dataRouter.js
+++ Routers/dataRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();  
-const db = require('./routerModel.js')
+const db = require('./routerModel.js') 
+
 
 router.get('/' ,(req , res) => {
     db.getAllProjects()
